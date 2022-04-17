8779a8115eed5801309de28f2ad8c0ba9460d50e
Umidjon <mrumidjon2020@gmail.com>
2022-04-17 20:25:29 +0500

    key(list): list of keys value(list): list of values

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,16 +1,15 @@
 def create_dictionary(key, value):
     """
-    Convert two lists into a dictionary
     Args:
         key(list): list of keys
         value(list): list of values
     Returns:
         dict: dictionary with keys and values
     """
-    res = {}
-    res.update(zip(key, value))
-    return res
-    # return list(zip(key, value))
+    # res = {}
+    # res.update(zip(key, value))
+    # return res
+    return dict(zip(key, value))
 
 
 print(create_dictionary([1, 2, 3], ["one", "two", "three"]))
