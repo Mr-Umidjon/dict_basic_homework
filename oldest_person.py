5ad96246a318e954ad01a54e4aefb787df50129d
Umidjon <mrumidjon2020@gmail.com>
2022-04-17 20:23:11 +0500

    Given a dictionary containing the names and ages of a group of people, return the name of the oldest person 01

--- oldest_person.py
+++ oldest_person.py
@@ -1,4 +1,4 @@
-def oldest(people:dict):
+def oldest(people: dict):
     """
     Given a dictionary containing the names and ages of a group of people, return the name of the oldest person.
     Args:
@@ -6,4 +6,7 @@ def oldest(people:dict):
     Returns:
         str: the name of the oldest person
     """
-    return
+    return max(people, key=people.get)
+
+
+print(oldest({"Javohir": 22, "Sharof": 23, "Tolib": 34, "Rustam": 16}))
